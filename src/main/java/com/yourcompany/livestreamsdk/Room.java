5173364266f8759dc1fa80096547eb4371c6fb96
JackalsonEnterprise-1998 <jackalsonadmob46664@gmail.com>
2025-11-27 18:28:53 -0800

    Update Room.java

--- src/main/java/com/yourcompany/livestreamsdk/Room.java
+++ src/main/java/com/yourcompany/livestreamsdk/Room.java
@@ -1,1 +1,15 @@
+package com.yourcompany.livestreamsdk;
+
+public class Room {
+    private String roomId;
+    private String hostId;
+
+    public Room(String roomId, String hostId) {
+        this.roomId = roomId;
+        this.hostId = hostId;
+    }
+
+    public String getRoomId() { return roomId; }
+    public String getHostId() { return hostId; }
+}
 
